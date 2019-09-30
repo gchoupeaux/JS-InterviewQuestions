a4e73deab59ea85dbd37d35c39fc9888feabec56
Guillaume Choupeaux <guillaume_choupeaux@apple.com>
2019-09-30 14:52:20 -0700

    reverseString

---
+++ reverseString.js
@@ -0,0 +1,25 @@
+// create a fucntion that reverse a string
+// input "abc"
+// output "cba"
+
+function reverse(str){
+	var strOut = ""
+	for (i = (str.length -1); i >= 0; i--){
+		strOut += str[i]
+	}
+	return strOut
+}
+
+function reverse1(str){
+	var strOut = []
+	for (i = 0; i < str.length; i++){
+		strOut.push(str[i])
+	}
+	return strOut.reverse().join('')
+}
+
+function reverse2(str){
+	return str.split('').reverse().join('')
+}
+
+reverse("baba")
